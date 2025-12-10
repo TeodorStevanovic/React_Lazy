d74b9c5f9609486e3d779f79ecb4f2056027c733
Teodor <stevanovicteodor002@gmail.com>
2025-12-10 11:05:41 +0100

    Added spinner fallback for lazy-loaded component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
+import { FadeLoader } from "react-spinners";
 import MyPage from "./MyPage";
 
 function App() {
   return (
-    <Suspense fallback={"loading..."}>
+    <Suspense fallback={<FadeLoader color={"lightblue"} size={150} />}>
       <MyPage />
     </Suspense>
   );
